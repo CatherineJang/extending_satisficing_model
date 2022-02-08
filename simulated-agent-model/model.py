6d6bbcd706a09617cdbf5f57042f49aaee752ab9
Eric Thompson-Martin <ethompsonmartin@g.hmc.edu>
2022-02-07 16:04:22 -0800

    Very basic model implimentation (no party ideology choice yet)

---
+++ simulated-agent-model/model.py
@@ -0,0 +1,111 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+def main(args):
+  runModel(args.sigmaHat, args.rationalizationFactor, args.numVoters, args.doPlot, args.doVoters, args.iterations)
+
+def runModel(sigmaHat, rationalizationFactor, numVoters, doPlot=False, doVoters=False, iterations=10):
+  population = np.random.normal(loc=0, scale=1, size = numVoters)
+  partyMeanInitialGuess = 1
+  if doPlot:
+    plt.hist(population,list(map(lambda x: (x-25)/10, range(50))),color=(0.0,0,1,0.1/iterations))
+  print(population)
+  for i in range(1,iterations+1):
+    population = iteratePopulation(population, sigmaHat, rationalizationFactor, partyMeanInitialGuess)
+    if doPlot:
+      plt.hist(population,list(map(lambda x: (x-25)/10, range(50))),color=(0.0,i/iterations,(1-i/iterations),(0.1+i/10)/iterations))
+  if doPlot:
+    plt.show()
+  print(population)
+
+gr = (math.sqrt(5) + 1) / 2
+
+def graph(formula, x, hue, b):
+    y = list(map(lambda x: formula(x/100), x))
+    x = x + list(map(lambda a: 0-a, x[-2::-1]))
+    y = y + y[-2::-1]
+    plt.plot(x, y, color=(int(b), hue, int(not b)), linewidth=1)
+
+def gss(f, a, b, tol=1e-3):
+    """Golden-section search
+    to find the minimum of f on [a,b]
+    f: a strictly unimodal function on [a,b]
+
+    Example:
+    >>> f = lambda x: (x-2)**2
+    >>> x = gss(f, 1, 5)
+    >>> print("%.15f" % x)
+    2.000009644875678
+
+    """
+    c = b - (b - a) / gr
+    d = a + (b - a) / gr
+    while abs(b - a) > tol:
+        if f(c) < f(d):
+            b = d
+        else:
+            a = c
+
+        # We recompute both c and d here to avoid loss of precision which may lead to incorrect results or infinite loop
+        c = b - (b - a) / gr
+        d = a + (b - a) / gr
+
+    return (b + a) / 2
+
+def iteratePopulation(population, sigmaHat, rationalizationFactor, partyMeanInitialGuess):
+  """
+    population is a vector of ideologies of people in the population.
+    Returns the new population
+  """
+
+  # TODO: Step 1
+  partyMean = partyMeanInitialGuess
+
+  # Step 2
+
+  # Gaussians for proportion of people at an ideology who are satisfied by each party and both
+  party1SatisficeProbs = np.array(list(map(lambda x: math.exp(0-(x-partyMean)**2/sigmaHat/2), population)))
+  party2SatisficeProbs = np.array(list(map(lambda x: math.exp(0-(x+partyMean)**2/sigmaHat/2), population)))
+
+  party1SatisficeUniform = np.random.uniform(size=population.shape)
+  party2SatisficeUniform = np.random.uniform(size=population.shape)
+  party1Satisfice = party1SatisficeProbs > party1SatisficeUniform
+  party2Satisfice = party2SatisficeProbs > party2SatisficeUniform
+
+  notBothSatisfice = np.logical_not(party1Satisfice & party2Satisfice)
+
+  tieBreakerUniform = np.random.uniform(size=population.shape)
+
+  # Find all voters for each party
+  party1Voters = party1Satisfice & (notBothSatisfice | (tieBreakerUniform > 0.5))
+  party2Voters = party2Satisfice & (notBothSatisfice | (tieBreakerUniform < 0.5))
+
+  # Step 3 & 4
+
+  votes = [0,0]
+  for i in range(population.shape[0]):
+    if party1Voters[i]:
+      population[i] += ((rationalizationFactor-1)/rationalizationFactor)*(partyMean - population[i])
+      votes[0]+=1
+    if party2Voters[i]:
+      population[i] += ((rationalizationFactor-1)/rationalizationFactor)*(0-partyMean - population[i])
+      votes[1]+=1
+  print(votes)
+
+  return population
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sigmaHat", help = "sigmaHat", type=float)
+    parser.add_argument("rationalizationFactor", help = "rationalization factor", type=float)
+    parser.add_argument("iterations", help="Number of iterations per simulation.", type=int)
+    parser.add_argument("numVoters", help="Number of agents to initialize.", type=int)
+    parser.add_argument("numSimulations", help="Number of simulations to run.", type=int)    
+    parser.add_argument("--doPlot", "-p", help="Name of file to write plot to.", action="store_true")
+    parser.add_argument("--doVoters", "-v", help="Plot voter curves.", action="store_true")
+    parser.add_argument("--symmetrical", "-s", help="Constrain voter ideologies to be symetrical.", action="store_true")
+    args = parser.parse_args()
+    main(args)

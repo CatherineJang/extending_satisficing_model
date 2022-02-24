34eabc7b897e7963089a6b3b271f1d0b5eca393e
Eric Thompson-Martin <ethompsonmartin@g.hmc.edu>
2022-02-24 15:09:41 -0800

    Add party to ideological mean mechanism for gifs

--- simulated-agent-model/gifs.py
+++ simulated-agent-model/gifs.py
@@ -5,14 +5,14 @@ import os
 
 def main(args):
     # make plots in folder (command: python3 gifs.py sigmaHat r-val iterations voters numSim -p -v -s)
-    runModel(args.sigmaHat, args.rationalizationFactor, args.numVoters, args.doPlot, args.iterations)
+    runModel(args.sigmaHat, args.rationalizationFactor, args.numVoters, args.doPlot, args.iterations, args.toMean)
     print("running here")
     # making gif
-    makeGif(args.sigmaHat, args.rationalizationFactor, args.numVoters, args.iterations)
+    makeGif(args.sigmaHat, args.rationalizationFactor, args.numVoters, args.iterations, args.toMean)
     print("done")
 
 
-def makeGif(sigmaHat, rationalizationFactor, numVoters, iterations):
+def makeGif(sigmaHat, rationalizationFactor, numVoters, iterations, toMean):
     # make filelist
     filelist = []
     for i in range(iterations+1):
@@ -20,7 +20,7 @@ def makeGif(sigmaHat, rationalizationFactor, numVoters, iterations):
 
     # build gif
     print("compiling voter curve gif...")
-    name = '../figs/simulation-figs/gifs/votercurve-{}-{}.gif'.format(sigmaHat,rationalizationFactor)
+    name = '../figs/simulation-figs/gifs/{}votercurve-{}-{}.gif'.format('m-' if toMean else '', sigmaHat,rationalizationFactor)
     with io.get_writer(name, mode='I') as writer:
         for idx, filename in enumerate(filelist):
             image = io.imread(filename)
@@ -41,6 +41,7 @@ if __name__ == "__main__":
     parser.add_argument("--doVoters", "-v", help="Plot voter curves.", action="store_true")
     parser.add_argument("--symmetrical", "-s", help="Constrain voter ideologies to be symetrical.", action="store_true")
     parser.add_argument("--parallell", "-l", help="Parallellize running of the simulation (will be fast, and your computer will heat up).", action="store_true")
+    parser.add_argument("--toMean", "-m", help="Parties go to their voter bases mean instead of vote maximizing ideology.", action="store_true")
     args = parser.parse_args()
     main(args)
 

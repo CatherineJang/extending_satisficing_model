6be457a36a6781d170c92d67be2a58fc54249f38
Eric Thompson-Martin <ethompsonmartin@g.hmc.edu>
2022-03-02 17:09:15 -0800

    Cleanup and invert y axis on heatmap

--- simulated-agent-model/convergenceHeatmap.py
+++ simulated-agent-model/convergenceHeatmap.py
@@ -1,3 +1,4 @@
+from ast import parse
 import seaborn as sns; sns.set_theme()
 import matplotlib.pyplot as plt
 import argparse
@@ -5,10 +6,9 @@ import pandas as pd
 
 from model import runModel
 
-from matplotlib.colors import LogNorm, Normalize
-from matplotlib.ticker import MaxNLocator
+from matplotlib.colors import LogNorm
 
-fineHeatMap = True
+fineHeatMap = False
 if fineHeatMap:
   defaultSigmaHRange = [x/20.0 for x in range(1, 61, 1)]
   defaultrfRange = [x/20.0 for x in range(21, 100, 1)]
@@ -17,15 +17,27 @@ else:
   defaultrfRange = [x/10.0 for x in range(11, 25, 4)]
 
 def main(args):
-  DF = makeDFs(args.numVoters, iterations=args.iterations)
-  sns.heatmap(DF, cmap="YlGnBu", norm=LogNorm(), cbar_kws={'ticks': [5,10,20,40,60,80,100], 'format':'%.i'})
+  if args.loadFile == None:
+    DF = makeDFs(args.numVoters, iterations=args.iterations, toMean=args.toMean)
+
+    filePath = '../figs/simulation-figs/Heatmaps/{}-{}-iter-Heat'.format(args.numVoters, args.iterations)
+
+    # save and read back in data
+    DF.to_csv(filePath+'.csv')
+  else:
+    filePath = '../figs/simulation-figs/Heatmaps/' + args.loadFile
+  DFLoad = pd.read_csv(filePath+'.csv', header=0, index_col=0)
+
+  sns.heatmap(DFLoad, cmap="YlGnBu", norm=LogNorm(), cbar_kws={'ticks': [10,15,20,30,40,60,80,100], 'format':'%.i'})
   plt.xlabel("rationalization factor")
   plt.ylabel("sigma_h")
-  plt.savefig('../figs/simulation-figs/Heatmaps/{}-{}-iter-Heat.png'.format(args.numVoters, args.iterations))
+  ax = plt.gca()
+  ax.invert_yaxis()
+  plt.savefig(filePath+'.png')
   plt.clf()
 
-def makeDFs(numVoters, sigmaHRange=defaultSigmaHRange, rfRange=defaultrfRange, iterations=3):
-  numIterToConverge = [[runModel(x, y, numVoters, iterations=iterations, convergence=True) for y in rfRange] for x in sigmaHRange]
+def makeDFs(numVoters, sigmaHRange=defaultSigmaHRange, rfRange=defaultrfRange, iterations=3, toMean=False):
+  numIterToConverge = [[runModel(x, y, numVoters, iterations=iterations, convergence=True, toMean=toMean) for y in rfRange] for x in sigmaHRange]
   
   # Make pandas DF for plot
   colLabels = list(map(str, rfRange))
@@ -37,6 +49,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("iterations", help="Number of iterations per simulation.", type=int)
     parser.add_argument("numVoters", help="Number of agents to initialize.", type=int)
+    parser.add_argument("--loadFile", help="If given load data from csv file, otherwise run model. Give as just filename in the Heatmaps directory, w/o '.csv'")
     parser.add_argument("--toMean", "-m", help="Parties go to their voter bases mean instead of vote maximizing ideology.", action="store_true")
     args = parser.parse_args()
     main(args)
